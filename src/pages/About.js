1650dc32baa828753e6222b6e56413d8987efe3a
annaliuu <liuanna2002@gmail.com>
2022-06-15 22:46:49 -0700

    finished lab2

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,27 +1,28 @@
 import React, { Component } from "react";
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
+import profile from "../assets/headshot.jpg";
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
         <div class="split left">
           <div className="centered">
             <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
+              className = "profile_image"
+              // Image goes here
+              src = {profile} alt="pfp"
             ></img>
           </div>
         </div>
         <div className="split right">
           <div className="centered">
-            <div className="name_title">Your Name</div>
+            <div className="name_title">Anna Liu</div>
             <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+              Hello, I'm Anna and I'm a third year data science student at UC
+              San Diego! Some of my hobbies include exploring food places with
+              friends, going on spontaneous trips and adventures, and
+              skiing/snowboarding during the winter :)
             </div>
           </div>
         </div>
